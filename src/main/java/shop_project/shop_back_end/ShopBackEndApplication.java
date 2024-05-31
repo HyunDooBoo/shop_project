6d55002467011c16a52574881504c0ac90ce4b0c
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-06-01 01:39:11 +0900

    feat: 프로젝트 생성 및 DB 연결

---
+++ src/main/java/shop_project/shop_back_end/ShopBackEndApplication.java
@@ -0,0 +1,13 @@
+package shop_project.shop_back_end;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShopBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShopBackEndApplication.class, args);
+	}
+
+}
---
+++ src/test/java/shop_project/shop_back_end/ShopBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package shop_project.shop_back_end;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
