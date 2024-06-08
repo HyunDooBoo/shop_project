69122c9bead9fe4820a6b74f000463836cd1b2bc
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-06-09 01:18:52 +0900

    feat: UserRequest 필수값 설정

--- src/main/java/shop_project/shop_back_end/web/dto/user/UserRequest.java
+++ src/main/java/shop_project/shop_back_end/web/dto/user/UserRequest.java
@@ -1,5 +1,7 @@
 package shop_project.shop_back_end.web.dto.user;
 
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
 import lombok.Getter;
 import lombok.Setter;
 import shop_project.shop_back_end.util.Sex;
@@ -8,14 +10,28 @@ import shop_project.shop_back_end.util.Sex;
 @Setter
 public class UserRequest {
 
+    @NotEmpty(message = "회원 이름은 필수입니다.")
     private String username;
+
+    @NotEmpty(message = "닉네임은 필수입니다.")
     private String nickname;
+
+    @NotEmpty(message = "비밀번호는 필수입니다.")
     private String password;
+
+    @NotNull(message = "나이 입력은 필수입니다.")
     private int age;
-    private String phoneNum;
+
+    @NotEmpty(message = "이메일 입력은 필수입니다.")
     private String email;
+
+    @NotEmpty(message = "핸드폰 번호 입력은 필수입니다.")
+    private String phoneNum;
+
     private String city;
     private String street;
     private String zipcode;
+
+    @NotNull(message = "성별 입력은 필수입니다.")
     private Sex sex;
 }
