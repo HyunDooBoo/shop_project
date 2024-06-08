af21a018ea950b6e39c5016ba4401caf30008249
HyunDooBoo <114637614+HyunDooBoo@users.noreply.github.com>
2024-06-09 01:25:21 +0900

    refactor: @Repository 어노테이션 삭제

--- src/main/java/shop_project/shop_back_end/domain/user/repository/UserRepository.java
+++ src/main/java/shop_project/shop_back_end/domain/user/repository/UserRepository.java
@@ -1,12 +1,10 @@
 package shop_project.shop_back_end.domain.user.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 import shop_project.shop_back_end.domain.user.User;
 
-@Repository
 public interface UserRepository extends JpaRepository<User, Long> {
 
-    boolean existsByNickname(String nickName);
+    boolean existsByNickname(String nickname);
 
 }
